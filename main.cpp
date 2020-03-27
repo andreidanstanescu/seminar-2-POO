b8f06c2fe40b72794057023f505a8cfd505bf4b3
andreidanstanescu <62710159+andreidanstanescu@users.noreply.github.com>
2020-03-27 16:56:50 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include "bst.hpp"
+#include "bst.cpp"
+
+int main ()
+{
+    BST tree,tree1;
+    tree.insert(5);
+    tree.insert(6);
+    tree.insert(4);
+    tree.insert(8);
+
+    tree1.insert(1);
+    tree1.insert(100);
+    tree1.insert(1123);
+    tree1.insert(1123);
+    tree1.insert(133);
+    tree1.insert(1345);
+    tree1.insert(111);
+    
+
+    //tree.afisare();
+
+    BST newtree = tree1 + tree;
+    newtree.afisare();
+    auto s = BST::inordine(newtree);
+    for (auto x : s) std::cout << x << ' ';
+}
